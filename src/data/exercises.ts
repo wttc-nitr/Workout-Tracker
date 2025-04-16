6f4301415300b6ef5f0d588f1fdbb1753ade4a4a
Animesh Kumar <kumaranimesh097@gmail.com>
2025-04-16 18:29:49 +0530

    add different exercises with targeted muscle

---
+++ src/data/exercises.ts
@@ -0,0 +1,247 @@
+export default [
+  {
+    id: "bench-press",
+    name: "Bench Press",
+    muscle: "Chest",
+  },
+  {
+    id: "squat",
+    name: "Squat",
+    muscle: "Legs",
+  },
+  {
+    id: "deadlift",
+    name: "Deadlift",
+    muscle: "Back",
+  },
+  {
+    id: "overhead-press",
+    name: "Overhead Press",
+    muscle: "Shoulders",
+  },
+  {
+    id: "pull-up",
+    name: "Pull-up",
+    muscle: "Back",
+  },
+  {
+    id: "barbell-row",
+    name: "Barbell Row",
+    muscle: "Back",
+  },
+  {
+    id: "dumbbell-curl",
+    name: "Dumbbell Curl",
+    muscle: "Biceps",
+  },
+  {
+    id: "tricep-pushdown",
+    name: "Tricep Pushdown",
+    muscle: "Triceps",
+  },
+  {
+    id: "leg-press",
+    name: "Leg Press",
+    muscle: "Legs",
+  },
+  {
+    id: "lat-pulldown",
+    name: "Lat Pulldown",
+    muscle: "Back",
+  },
+  {
+    id: "dumbbell-fly",
+    name: "Dumbbell Fly",
+    muscle: "Chest",
+  },
+  {
+    id: "romanian-deadlift",
+    name: "Romanian Deadlift",
+    muscle: "Hamstrings",
+  },
+  {
+    id: "leg-extension",
+    name: "Leg Extension",
+    muscle: "Quadriceps",
+  },
+  {
+    id: "leg-curl",
+    name: "Leg Curl",
+    muscle: "Hamstrings",
+  },
+  {
+    id: "calf-raise",
+    name: "Calf Raise",
+    muscle: "Calves",
+  },
+  {
+    id: "dips",
+    name: "Dips",
+    muscle: "Chest",
+  },
+  {
+    id: "face-pull",
+    name: "Face Pull",
+    muscle: "Shoulders",
+  },
+  {
+    id: "hammer-curl",
+    name: "Hammer Curl",
+    muscle: "Biceps",
+  },
+  {
+    id: "incline-bench-press",
+    name: "Incline Bench Press",
+    muscle: "Upper Chest",
+  },
+  {
+    id: "lateral-raise",
+    name: "Lateral Raise",
+    muscle: "Shoulders",
+  },
+  {
+    id: "lunges",
+    name: "Lunges",
+    muscle: "Legs",
+  },
+  {
+    id: "plank",
+    name: "Plank",
+    muscle: "Core",
+  },
+  {
+    id: "push-up",
+    name: "Push-up",
+    muscle: "Chest",
+  },
+  {
+    id: "seated-cable-row",
+    name: "Seated Cable Row",
+    muscle: "Back",
+  },
+  {
+    id: "shoulder-press",
+    name: "Shoulder Press",
+    muscle: "Shoulders",
+  },
+  {
+    id: "shrug",
+    name: "Shrug",
+    muscle: "Traps",
+  },
+  {
+    id: "skull-crusher",
+    name: "Skull Crusher",
+    muscle: "Triceps",
+  },
+  {
+    id: "t-bar-row",
+    name: "T-Bar Row",
+    muscle: "Back",
+  },
+  {
+    id: "upright-row",
+    name: "Upright Row",
+    muscle: "Shoulders",
+  },
+  {
+    id: "arnold-press",
+    name: "Arnold Press",
+    muscle: "Shoulders",
+  },
+  {
+    id: "barbell-curl",
+    name: "Barbell Curl",
+    muscle: "Biceps",
+  },
+  {
+    id: "cable-crossover",
+    name: "Cable Crossover",
+    muscle: "Chest",
+  },
+  {
+    id: "chin-up",
+    name: "Chin-up",
+    muscle: "Back",
+  },
+  {
+    id: "decline-bench-press",
+    name: "Decline Bench Press",
+    muscle: "Lower Chest",
+  },
+  {
+    id: "dumbbell-press",
+    name: "Dumbbell Press",
+    muscle: "Chest",
+  },
+  {
+    id: "ez-bar-curl",
+    name: "EZ Bar Curl",
+    muscle: "Biceps",
+  },
+  {
+    id: "front-raise",
+    name: "Front Raise",
+    muscle: "Shoulders",
+  },
+  {
+    id: "hack-squat",
+    name: "Hack Squat",
+    muscle: "Legs",
+  },
+  {
+    id: "hip-thrust",
+    name: "Hip Thrust",
+    muscle: "Glutes",
+  },
+  {
+    id: "inverted-row",
+    name: "Inverted Row",
+    muscle: "Back",
+  },
+  {
+    id: "kettlebell-swing",
+    name: "Kettlebell Swing",
+    muscle: "Full Body",
+  },
+  {
+    id: "machine-chest-press",
+    name: "Machine Chest Press",
+    muscle: "Chest",
+  },
+  {
+    id: "pec-deck",
+    name: "Pec Deck",
+    muscle: "Chest",
+  },
+  {
+    id: "preacher-curl",
+    name: "Preacher Curl",
+    muscle: "Biceps",
+  },
+  {
+    id: "reverse-fly",
+    name: "Reverse Fly",
+    muscle: "Rear Deltoids",
+  },
+  {
+    id: "seated-leg-press",
+    name: "Seated Leg Press",
+    muscle: "Legs",
+  },
+  {
+    id: "smith-machine-squat",
+    name: "Smith Machine Squat",
+    muscle: "Legs",
+  },
+  {
+    id: "sumo-deadlift",
+    name: "Sumo Deadlift",
+    muscle: "Legs",
+  },
+  {
+    id: "tricep-extension",
+    name: "Tricep Extension",
+    muscle: "Triceps",
+  },
+];
