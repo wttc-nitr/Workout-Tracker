e298178105c3820f10637ebd8ee63a79de3122c9
Animesh Kumar <kumaranimesh097@gmail.com>
2025-04-23 21:59:51 +0530

    add examples to understand types

--- src/types/models.ts
+++ src/types/models.ts
@@ -19,10 +19,83 @@ export type ExerciseSet = {
 };
 
 // extras, for nested structure
+export type ExerciseWithSets = Exercise & {
+  sets: ExerciseSet[];
+};
+
+/*
+id: string;
+workoutId: string;
+name: string;
+sets: [
+  {
+    id: string;
+    exerciseId: string;
+    reps?: number;
+    weight?: number;
+    oneRM?: number;
+  },
+  {
+    id: string;
+    exerciseId: string;
+    reps?: number;
+    weight?: number;
+    oneRM?: number;
+  }
+]
+*/
+
 export type WorkoutWithExercises = Workout & {
   exercises: ExerciseWithSets[];
 };
 
-export type ExerciseWithSets = Exercise & {
-  sets: ExerciseSet[];
-};
+/**
+id: string;
+createdAt: Date;
+finishedAt: Date | null;
+
+exercises: [
+  {
+    id: string;
+    workoutId: string;
+    name: string;
+    sets: [
+      {
+        id: string;
+        exerciseId: string;
+        reps?: number;
+        weight?: number;
+        oneRM?: number;
+      },
+      {
+        id: string;
+        exerciseId: string;
+        reps?: number;
+        weight?: number;
+        oneRM?: number;
+      }
+    ]
+  },
+  {
+    id: string;
+    workoutId: string;
+    name: string;
+    sets: [
+      {
+        id: string;
+        exerciseId: string;
+        reps?: number;
+        weight?: number;
+        oneRM?: number;
+      },
+      {
+        id: string;
+        exerciseId: string;
+        reps?: number;
+        weight?: number;
+        oneRM?: number;
+      }
+    ]
+  }
+]
+*/
