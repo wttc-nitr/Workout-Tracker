833d44cfe9c37a02fb9d81693faec92872b6f8e1
Animesh Kumar <kumaranimesh097@gmail.com>
2025-04-24 23:25:43 +0530

    improve workout-id screen to show details of a workout

--- src/app/workout/[id].tsx
+++ src/app/workout/[id].tsx
@@ -1,11 +1,44 @@
 import { useLocalSearchParams } from "expo-router";
-import { Text, View } from "@/components/general/Themed";
+import { Text } from "@/components/general/Themed";
+import dummyWorkouts from "@/data/dummyWorkouts";
+import WorkoutExerciseItem from "@/components/workouts/WorkoutExerciseItem";
+import { FlatList, StyleSheet } from "react-native";
+import dayjs from "dayjs";
 
 export default function WorkoutScreen() {
   const { id } = useLocalSearchParams();
+
+  const workout = dummyWorkouts.find((w) => w.id === id);
+
+  if (!workout) {
+    return <Text>Workout not found</Text>;
+  }
+
   return (
-    <View style={{ flex: 1 }}>
-      <Text>Workout screen: {id}</Text>
-    </View>
+    <FlatList
+      data={workout.exercises}
+      contentContainerStyle={{ gap: 8, padding: 8 }}
+      renderItem={({ item }) => <WorkoutExerciseItem exercise={item} />}
+      ListHeaderComponent={
+        <>
+          <Text style={styles.title}>Workout details</Text>
+          <Text style={styles.date}>
+            {dayjs(workout.createdAt).format("HH:mm dddd, D MMM")}
+          </Text>
+        </>
+      }
+    />
   );
 }
+
+const styles = StyleSheet.create({
+  title: {
+    fontSize: 28,
+    fontWeight: "bold",
+    marginBottom: 10,
+  },
+  date: {
+    fontSize: 18,
+    marginBottom: 20,
+  },
+});
