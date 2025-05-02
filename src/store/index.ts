656da6435bb475ed35b59a68bc60188ecc49d19a
Animesh Kumar <kumaranimesh097@gmail.com>
2025-05-02 14:42:14 +0530

    create bare-bone Zustand store

---
+++ src/store/index.ts
@@ -0,0 +1,12 @@
+import { create } from "zustand";
+
+type State = {};
+
+type Actions = {};
+
+export const useWorkouts = create<State & Actions>()((state) => {
+  return {
+    // State
+    // Actions
+  };
+});
