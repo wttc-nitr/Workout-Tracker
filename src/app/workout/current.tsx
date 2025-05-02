8df30c559a6082d8510b45b2fe6e2f14fde4cc28
Animesh Kumar <kumaranimesh097@gmail.com>
2025-05-03 00:57:31 +0530

    replace FlatList with KeyboardAwareFlatList, make it render actual data

--- src/app/workout/current.tsx
+++ src/app/workout/current.tsx
@@ -1,32 +1,25 @@
 import WorkoutExerciseItem from "@/components/logger/WorkoutExerciseItem";
-import { FlatList } from "react-native";
 import WorkoutHeader from "@/components/logger/WorkoutHeader";
 import SelectExerciseModal from "@/components/logger/SelectExerciseModal";
-import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
+import { KeyboardAwareFlatList } from "react-native-keyboard-aware-scroll-view";
 import { useWorkouts } from "@/store";
 import { Redirect } from "expo-router";
 
 export default function CurrentWorkoutScreen() {
   const currentWorkout = useWorkouts((state) => state.currentWorkout);
-
+  const addExercise = useWorkouts((state) => state.addExercise);
   if (!currentWorkout) {
     return <Redirect href={"/"} />;
   }
   return (
-    <KeyboardAwareScrollView>
-      <FlatList
-        data={[1, 2, 3]}
-        contentContainerStyle={{ gap: 10, padding: 10 }}
-        renderItem={() => <WorkoutExerciseItem />}
-        ListHeaderComponent={<WorkoutHeader />}
-        ListFooterComponent={
-          <SelectExerciseModal
-            onSelectExercise={(name) =>
-              console.warn("Exercise seleted: ", name)
-            }
-          />
-        }
-      />
-    </KeyboardAwareScrollView>
+    <KeyboardAwareFlatList
+      data={currentWorkout.exercises}
+      contentContainerStyle={{ gap: 10, padding: 10 }}
+      renderItem={({ item }) => <WorkoutExerciseItem exercise={item} />}
+      ListHeaderComponent={<WorkoutHeader />}
+      ListFooterComponent={
+        <SelectExerciseModal onSelectExercise={(name) => addExercise(name)} />
+      }
+    />
   );
 }
