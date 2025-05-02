6ca245901048cda142af5b8aa1455074fd63ae61
Animesh Kumar <kumaranimesh097@gmail.com>
2025-05-02 19:03:36 +0530

    add other State & Actions in Zustand store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,12 +1,41 @@
+// responsible for Storing data only, everything else should be moved to appropriate folders.
+
+import type { WorkoutWithExercises } from "@/types/models";
 import { create } from "zustand";
+import { createNewWorkout, finishWorkout } from "@/services/workoutService";
 
-type State = {};
+type State = {
+  currentWorkout: WorkoutWithExercises | null;
+  workouts: WorkoutWithExercises[];
+};
 
-type Actions = {};
+type Actions = {
+  startWorkout: () => void;
+  endWorkout: () => void;
+};
 
-export const useWorkouts = create<State & Actions>()((state) => {
+export const useWorkouts = create<State & Actions>()((set, get) => {
   return {
     // State
+    currentWorkout: null,
+    workouts: [],
+
     // Actions
+    startWorkout: () => {
+      set((state) => ({ currentWorkout: createNewWorkout() }));
+    },
+
+    endWorkout: () => {
+      // end this current workout -> currentWorkout to null, push this workout to the list
+      const { currentWorkout } = get();
+      if (!currentWorkout) return;
+
+      const finishedWorkout = finishWorkout(currentWorkout);
+
+      set((state) => ({
+        currentWorkout: null,
+        workouts: [...state.workouts, finishedWorkout],
+      }));
+    },
   };
 });
