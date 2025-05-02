d5255abbb29cfdc9f607190d38bf289dcbe45e86
Animesh Kumar <kumaranimesh097@gmail.com>
2025-05-02 19:00:49 +0530

    add create-workout & fnsh-workout, and install expo-crypto for generating random UUIDs

--- src/services/workoutService.ts
+++ src/services/workoutService.ts
@@ -1,9 +1,30 @@
 import type { WorkoutWithExercises } from "@/types/models";
 import { getExerciseTotalWeight } from "@/services/exerciseService";
+import * as Crypto from "expo-crypto";
 
 export const getWorkoutTotalWeight = (workout: WorkoutWithExercises) => {
   return workout.exercises.reduce(
     (total, exercise) => total + getExerciseTotalWeight(exercise),
     0,
   );
+};
+
+export const createNewWorkout = () => {
+  const newWorkout: WorkoutWithExercises = {
+    id: Crypto.randomUUID(),
+    createdAt: new Date(),
+    finishedAt: null,
+    exercises: [],
+  };
+
+  return newWorkout;
+};
+
+export const finishWorkout = (workout: WorkoutWithExercises) => {
+  const finishedWorkout: WorkoutWithExercises = {
+    ...workout,
+    finishedAt: new Date(),
+  };
+
+  return finishedWorkout;
 };
