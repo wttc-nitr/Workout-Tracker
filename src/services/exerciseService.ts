c0d653f5018e943c16ea1964a858da18acf75a4e
Animesh Kumar <kumaranimesh097@gmail.com>
2025-05-02 22:20:54 +0530

    add a service to create new-exercise

--- src/services/exerciseService.ts
+++ src/services/exerciseService.ts
@@ -1,9 +1,21 @@
 import type { ExerciseWithSets } from "@/types/models";
 import { getSetTotalWeight } from "@/services/setService";
+import * as Crypto from "expo-crypto";
 
 export const getExerciseTotalWeight = (exercise: ExerciseWithSets) => {
   return exercise.sets.reduce(
     (totalSetWeight, set) => totalSetWeight + getSetTotalWeight(set),
     0,
   );
+};
+
+export const createNewExercise = (name: string, workoutId: string) => {
+  const newExercise: ExerciseWithSets = {
+    id: Crypto.randomUUID(),
+    name,
+    workoutId,
+    sets: [],
+  };
+
+  return newExercise;
 };
