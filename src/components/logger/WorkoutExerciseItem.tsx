e03511a1521a1286f74bd7afac57f17a4f038f01
Animesh Kumar <kumaranimesh097@gmail.com>
2025-05-03 00:52:49 +0530

    remove dummy data

--- src/components/logger/WorkoutExerciseItem.tsx
+++ src/components/logger/WorkoutExerciseItem.tsx
@@ -2,34 +2,23 @@ import Card from "@/components/general/Card";
 import { View, Text } from "@/components/general/Themed";
 import { StyleSheet } from "react-native";
 import SetItem from "./SetItem";
-import type { ExerciseSet } from "@/types/models";
+import type { ExerciseSet, ExerciseWithSets } from "@/types/models";
 import CustomButton from "../general/CustomButton";
 
-export default function WorkoutExerciseItem() {
-  const sets: ExerciseSet[] = [
-    {
-      id: "1",
-      weight: 20,
-      reps: 10,
-      exerciseId: "e1",
-    },
-    {
-      id: "2",
-      weight: 50,
-      reps: 5,
-      exerciseId: "e1",
-    },
-  ];
+type WorkoutExerciseItem = {
+  exercise: ExerciseWithSets;
+};
 
+export default function WorkoutExerciseItem({ exercise }: WorkoutExerciseItem) {
   return (
-    <Card title="Exercise">
+    <Card title={exercise.name}>
       <View style={styles.header}>
         <Text style={styles.setNumber}>Set</Text>
         <Text style={styles.setInfo}>kg</Text>
         <Text style={styles.setInfo}>Reps</Text>
       </View>
       <View style={{ gap: 5 }}>
-        {sets.map((item, index) => (
+        {exercise.sets.map((item, index) => (
           <SetItem key={item.id} index={index} set={item} />
         ))}
       </View>
