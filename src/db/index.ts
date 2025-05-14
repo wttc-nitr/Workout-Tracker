cb7b04b324e1584040f29566e5f8906b7beeb7b7
Animesh Kumar <kumaranimesh097@gmail.com>
2025-05-15 00:56:35 +0530

    feat: implement workout table creation

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,13 +1,28 @@
 import * as SQLite from "expo-sqlite";
 
-let db: SQLite.SQLiteDatabase | null = null;
-
 export const dbName = "workoutTracker.db";
 
-export const getDB = async () => {
-  if (db) return db;
+let db: SQLite.SQLiteDatabase | null = null;
 
+const createWorkoutsTableQuery = `
+  CREATE TABLE IF NOT EXISTS workouts (
+    id TEXT PRIMARY KEY,
+    created_at TEXT,
+    finished_at TEXT
+  );`;
+
+export const getDB = async () => {
+  if (db) {
+    return db;
+  }
+
+  console.log("Opening database...");
   db = await SQLite.openDatabaseAsync(dbName);
+  console.log("Database opened");
+
+  console.log("Creating tables...");
+  await db.execAsync(createWorkoutsTableQuery);
+  console.log("Tables created");
 
   return db;
 };
