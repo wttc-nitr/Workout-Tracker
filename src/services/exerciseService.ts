81bfae61ab0df75bcff5fc4d700fbf1c63c6533a
Animesh Kumar <kumaranimesh097@gmail.com>
2025-05-25 14:30:21 +0530

    implement:
    - when an exercise is created, save it to db.
    - after cleanup, if exercise is empty (no sets), remove it from db.

--- src/services/exerciseService.ts
+++ src/services/exerciseService.ts
@@ -5,6 +5,7 @@ import {
   getSetTotalWeight,
 } from "@/services/setService";
 import * as Crypto from "expo-crypto";
+import { deleteExercise, saveExercise } from "@/db/exercises";
 
 export const getExerciseTotalWeight = (exercise: ExerciseWithSets) => {
   return exercise.sets.reduce(
@@ -21,6 +22,9 @@ export const createNewExercise = (name: string, workoutId: string) => {
     sets: [],
   };
 
+  // save to db
+  saveExercise(newExercise);
+  // add one empty set
   newExercise.sets.push(createNewSet(newExercise.id));
 
   return newExercise;
@@ -30,7 +34,10 @@ export const cleanExercise = (exercise: ExerciseWithSets) => {
   // remove empty sets -> sets with zero/no reps
   const cleanedSets = cleanSets(exercise.sets);
 
-  if (cleanedSets.length === 0) return null;
+  if (cleanedSets.length === 0) {
+    deleteExercise(exercise.id);
+    return null;
+  }
 
   return {
     ...exercise,
