caaf898de1dd7658fa719bfac45d21914b9a6aeb
Animesh Kumar <kumaranimesh097@gmail.com>
2025-09-15 13:29:15 +0530

    fix: status bar colors in dark mode
    In dark mode, Set status bar to black background with white text/icons

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -12,6 +12,8 @@ import { useDrizzleStudio } from "expo-drizzle-studio-plugin";
 import * as SQLite from "expo-sqlite";
 import { dbName } from "@/db";
 import { useEffect } from "react";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import { StatusBar } from "expo-status-bar";
 
 const db = SQLite.openDatabaseSync(dbName);
 // getDB();
@@ -29,31 +31,48 @@ export default function RootLayout() {
   }, []);
 
   return (
-    <GestureHandlerRootView style={{ flex: 1 }}>
-      <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
-        <Stack
-          screenOptions={{
-            headerTintColor: "blue",
-            headerTitleAlign: "center",
-          }}
-        >
-          <Stack.Screen name="index" options={{ title: "Home" }} />
-          <Stack.Screen
-            name="workout/current"
-            options={{
-              title: "Workout",
-              headerRight: () => (
-                <CustomButton
-                  onPress={finishWorkout}
-                  title="Finish"
-                  style={{ padding: 7, paddingHorizontal: 15, width: "auto" }}
-                />
-              ),
-            }}
-          />
-          <Stack.Screen name="workout/[id]" options={{ title: "Workout" }} />
-        </Stack>
-      </ThemeProvider>
-    </GestureHandlerRootView>
+    <SafeAreaProvider>
+      <StatusBar
+        backgroundColor={colorScheme === "dark" ? "black" : "white"}
+        style={colorScheme === "dark" ? "light" : "dark"}
+      />
+      <SafeAreaView style={{ flex: 1 }}>
+        <GestureHandlerRootView style={{ flex: 1 }}>
+          <ThemeProvider
+            value={colorScheme === "dark" ? DarkTheme : DefaultTheme}
+          >
+            <Stack
+              screenOptions={{
+                headerTintColor: "blue",
+                headerTitleAlign: "center",
+              }}
+            >
+              <Stack.Screen name="index" options={{ title: "Home" }} />
+              <Stack.Screen
+                name="workout/current"
+                options={{
+                  title: "Workout",
+                  headerRight: () => (
+                    <CustomButton
+                      onPress={finishWorkout}
+                      title="Finish"
+                      style={{
+                        padding: 7,
+                        paddingHorizontal: 15,
+                        width: "auto",
+                      }}
+                    />
+                  ),
+                }}
+              />
+              <Stack.Screen
+                name="workout/[id]"
+                options={{ title: "Workout" }}
+              />
+            </Stack>
+          </ThemeProvider>
+        </GestureHandlerRootView>
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
